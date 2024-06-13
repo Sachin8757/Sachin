2d487eeb76047d762b0eeacfc00a06adddeb08f5
Sachin kumar <130838646+sachinkuamr@users.noreply.github.com>
2024-06-13 12:59:49 +0530

    add new things

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,20 @@
 var express = require('express');
 var router = express.Router();
-const userModel = require('./data')
-
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
+  
 });
-router.get('/login', function(req, res) {
-  res.render('login');
+
+router.get('/project', function(req, res) {
+  // res.render('index');
+  res.send("this page is under creating")
+  
 });
-router.post("/login", function (req, res, next) {
-  const user= userModel.create({
-    username:req.body.username,
-    password:req.body.password
-  })
-  // res.send(user)
-  res.redirect('/recharge');
-})
 
-
-router.get('/recharge', function(req, res) {
-  res.render('recharge');
-
-});
-router.post("/recharge", function (req, res, next) {
-  res.redirect("/success");
-})
-
-router.get('/success', function(req, res) {
-  res.render('success');
-
+router.get('/resume', function(req, res) {
+  res.render('resume');
+  
 });
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+  
 });
+
 
 module.exports = router;
