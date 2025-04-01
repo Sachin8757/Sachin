4990294117e4143a08df70203db0ad5edfabf17b
Sachin kumar <130838646+sachinkuamr@users.noreply.github.com>
2025-04-01 12:07:05 +0530

    some data

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer=require('nodemailer')
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
   
 });
+router.post('/contact', function(req, res) {
+  let {name,email,yourtext}=req.body;
+  console.log(name,email,yourtext);
+  var transporter=nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+      user:'sachin875788@gmail.com ',
+      pass:'sdzc exvn dnsc edgp'
+    }
+  });
+  var mailOption={
+    form:email,
+    to:'sachin875788@gmail.com',
+    subject:email,
+    text:yourtext
+  }
+  transporter.sendMail(mailOption,function(error,info){
+    if(error){
+      console.log(error);
+    }else{
+      console.log("email send")
+    }
+    res.redirect("/")
+  })
 
-router.get('/project', function(req, res) {
-  res.render('project');
-  
+
 });
-
-router.get('/resume', function(req, res) {
-  res.render('resume');
-  
-});
-
-router.get('/contact', function(req, res) {
-  // res.render('resume');
-  res.send("work on Progress")
-  
-});
-router.get('/about', function(req, res) {
-  // res.render('resume');
-  res.send("work on Progress")
-});
-
-
-router.get('/skills', function(req, res) {
-  res.render('skills');
-});
-
-
-
 
 module.exports = router;
