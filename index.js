0cbe0a20c19a47149dc4743f6d5e9b59cfb4ab65
priyanshukumar72 <priyanshu727989@gmail.com>
2025-07-08 12:15:45 +0530

    edit project file

--- index.js
+++ index.js
@@ -19,29 +19,29 @@ app.get('/project', function(req, res) {
   
 });
 app.post('/contact', function(req, res) {
-  let {name,email,yourtext}=req.body;
-  console.log(name,email,yourtext);
-  var transporter=nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-      user:'sachin875788@gmail.com ',
-      pass:'sdzc exvn dnsc edgp'
-    }
-  });
-  var mailOption={
-    form:email,
-    to:'sachin875788@gmail.com',
-    subject:email,
-    text:yourtext
-  }
-  transporter.sendMail(mailOption,function(error,info){
-    if(error){
-      console.log(error);
-    }else{
-      console.log("email send")
-    }
-    res.redirect("/")
-  })
+  let {namee,email,yourtext}=req.body;
+  console.log(namee,email,yourtext);
+  // var transporter=nodemailer.createTransport({
+  //   service:"gmail",
+  //   auth:{
+  //     user:'sachin875788@gmail.com ',
+  //     pass:'sdzc exvn dnsc edgp'
+  //   }
+  // });
+  // var mailOption={
+  //   form:email,
+  //   to:'sachin875788@gmail.com',
+  //   subject:email,
+  //   text:yourtext
+  // }
+  // transporter.sendMail(mailOption,function(error,info){
+  //   if(error){
+  //     console.log(error);
+  //   }else{
+  //     console.log("email send")
+  //   }
+  //   res.redirect("/")
+  // })
 
 
 });
